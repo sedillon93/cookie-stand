3ca714594f95e0ade6a24b43ae5bae251a3e9ac6
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-14 14:24:17 -0700

    add hourly sales function

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var Alki = {
+var alki = {
   name: 'Alki',
   hours: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
   minCustomersHourly: 2,
   maxCustomersHourly: 16,
   avgCookiesPerCustomer: 4.6,
+  customersHourly: function(){
+    Math.floor(Math.random() * (maxCustomersHourly - minCustomersHourly) + minCustomersHourly);
+  },
   changeMinCustomers: function(newMin){
     this.minCustomersHourly = newMin;
   },
@@ -13,10 +16,17 @@ var Alki = {
   changeAvgCookies: function(newAvg){
     this.avgCookiesPerCustomer = newAvg;
   },
-  customersHourly: function(){
-    Math.floor(Math.random() * (maxCustomersHourly - minCustomersHourly) + minCustomersHourly);
-  },
   cookiesHourly: function(){
-    var hourlySales = this.customersHourly * this.avgCookiesPerCustomer;
+    for (var i = 0; i < this.hours.length; i++){
+      var hourlySales = [];
+      var hourTotal = this.customersHourly * this.avgCookiesPerCustomer;
+      if (this.hours[i] < 12){
+        var displayMessage = this.hours[i] + 'am: ' + hourTotal + ' cookies';
+      }
+      else{
+        var displayMessage = this.hours[i] + 'pm: ' + hourTotal + ' cookies';
+      };
+      hourlySales.push(hourTotal);
+    }
   }
 };
