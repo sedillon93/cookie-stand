3671ac672dc9ad566148520810afd3c6da0495c0
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-14 20:40:49 -0700

    add seatac object

--- app.js
+++ app.js
@@ -1,12 +1,86 @@
 'use strict';
 
-//start Capitol Hill store object, methods, & DOM manipulation
+//start SeaTac store object, methods, & DOM manipulation
+var seaTac = {
+  name: 'SeaTac Airport',
+  hours: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
+  minCustomersHourly: 3,
+  maxCustomersHourly: 24,
+  avgCookiesPerCustomer: 1.2,
+  hourlySales: [],
+  salesStatements: [],
+  totalCookiesSold: '',
+  // customersHourly returns a random number of customers
+  customersHourly: function(){
+    var customers = Math.floor(Math.random() * (this.maxCustomersHourly - this.minCustomersHourly) + this.minCustomersHourly);
+    return customers;
+  },
+
+  // cookiesHourly creates an array of the cookies sold per hour, each element corresponds to sales for one hour; stored in hourlySales
+  cookiesHourly: function(){
+    for (var i = 0; i < this.hours.length; i++){
+      var hourTotal = Math.round(this.customersHourly() * this.avgCookiesPerCustomer);
+      this.hourlySales.push(hourTotal);
+    }
+    return this.hourlySales;
+  },
+
+    // cookieSaleStatement creates an array of strings of the form 'time am: #ofcookies cookies'; stored in salesStatements
+  cookieSaleStatement: function(){
+    for (var i = 0; i < this.hourlySales.length; i++){
+      if (i < 6) {
+        var message = this.hours[i] + 'am: ' + this.hourlySales[i] + ' cookies';
+      }
+      else {
+        var message = this.hours[i] + 'pm: ' + this.hourlySales[i] + ' cookies';
+      }
+      this.salesStatements.push(message);
+    }
+    return this.salesStatements;
+  },
+
+  //add up all numbers in hourlySales array to get total number of cookies sold in a day at that location
+  totalCookies: function(){
+    var total = 0;
+    for (var i = 0; i < this.hourlySales.length; i++){
+      total = total + this.hourlySales[i];
+    }
+    this.totalCookiesSold = 'Total: ' + total + ' cookies';
+  }
+};
+
+seaTac.cookiesHourly();
+seaTac.cookieSaleStatement();
+seaTac.totalCookies();
+
+var body = document.getElementsByTagName('body')[0];
+var h1 = document.createElement('h1');
+h1.className = 'name';
+h1.innerText = seaTac.name;
+body.appendChild(h1);
+
+var ul = document.createElement('ul');
+ul.id = 'seaTac';
+body.appendChild(ul);
+
+for (var i = 0; i < seaTac.salesStatements.length; i++) {
+  var newLi = document.createElement('li');
+  newLi.innerText = seaTac.salesStatements[i];
+  ul.appendChild(newLi);
+}
+
+var allCookies = document.createElement('li');
+allCookies.innerText = seaTac.totalCookiesSold;
+ul.appendChild(allCookies);
+//end SeaTac object
+
+//start Seattle Center store object, methods, & DOM manipulation
 var seattleCenter = {
   name: 'Seattle Center',
   hours: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
-  minCustomersHourly: 20,
+  minCustomersHourly: 11,
   maxCustomersHourly: 38,
-  avgCookiesPerCustomer: 2.3,
+  avgCookiesPerCustomer: 3.7,
   hourlySales: [],
   salesStatements: [],
   totalCookiesSold: '',
