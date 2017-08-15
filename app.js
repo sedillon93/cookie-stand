41c7aba748b68584e9399d91d5f0823a8e6f871f
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-15 13:41:08 -0700

    start refactoring code

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
 'use strict';
 
-//start Capitol Hill store object, methods, & DOM manipulation
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+function Store(name, minCustomersHourly, maxCustomersHourly, avgCookiesPerCustomer){
+  this.name = name;
+  this.mincCustomersHourly = minCustomersHourly;
+  this.maxCustomersHourly = maxCustomersHourly;
+  this.avgCookiesPerCustomer = avgCookiesPerCustomer;
+  this.hourlyCookieSales = [];
+  customersHourly = function(){
+    var customers = Math.floor(Math.random() * (this.maxCustomersHourly - this.minCustomersHourly) + this.minCustomersHourly);
+    return customers;
+  };
+  cookiesHourly = function(){
+    hourlyCookieSales = [];
+    for (var i = 0; i < hours.length; i++){
+      var hourTotal = Math.round(this.customersHourly() * this.avgCookiesPerCustomer);
+      this.hourlyCookieSales.push(hourTotal);
+    }
+    return this.hourlyCookieSales;
+  };
+  customersHourly();
+  cookiesHourly();
+};
+
+//start 1st and Pike store object, methods, & DOM manipulation
 var pike = {
   name: '1st and Pike',
-  hours: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
   minCustomersHourly: 23,
   maxCustomersHourly: 65,
   avgCookiesPerCustomer: 6.3,
-  hourlySales: [],
+  hourlyCookieSalesy: [],
   salesStatements: [],
   totalCookiesSold: '',
-  // customersHourly returns a random number of customers
-  customersHourly: function(){
-    var customers = Math.floor(Math.random() * (this.maxCustomersHourly - this.minCustomersHourly) + this.minCustomersHourly);
-    return customers;
-  },
-
-  // cookiesHourly creates an array of the cookies sold per hour, each element corresponds to sales for one hour; stored in hourlySales
-  cookiesHourly: function(){
-    for (var i = 0; i < this.hours.length; i++){
-      var hourTotal = Math.round(this.customersHourly() * this.avgCookiesPerCustomer);
-      this.hourlySales.push(hourTotal);
-    }
-    return this.hourlySales;
-  },
 
     // cookieSaleStatement creates an array of strings of the form 'time am: #ofcookies cookies'; stored in salesStatements
   cookieSaleStatement: function(){
     for (var i = 0; i < this.hourlySales.length; i++){
-      if (i < 6) {
-        var message = this.hours[i] + 'am: ' + this.hourlySales[i] + ' cookies';
-      }
-      else {
-        var message = this.hours[i] + 'pm: ' + this.hourlySales[i] + ' cookies';
-      }
-      this.salesStatements.push(message);
+      return this.salesStatements;
     }
-    return this.salesStatements;
   },
 
   //add up all numbers in hourlySales array to get total number of cookies sold in a day at that location
@@ -77,7 +79,6 @@ ul.appendChild(allCookies);
 //start SeaTac store object, methods, & DOM manipulation
 var seaTac = {
   name: 'SeaTac Airport',
-  hours: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
   minCustomersHourly: 3,
   maxCustomersHourly: 24,
   avgCookiesPerCustomer: 1.2,
@@ -151,7 +152,6 @@ ul.appendChild(allCookies);
 //start Seattle Center store object, methods, & DOM manipulation
 var seattleCenter = {
   name: 'Seattle Center',
-  hours: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
   minCustomersHourly: 11,
   maxCustomersHourly: 38,
   avgCookiesPerCustomer: 3.7,
@@ -225,7 +225,6 @@ ul.appendChild(allCookies);
 //start Capitol Hill store object, methods, & DOM manipulation
 var capHill = {
   name: 'Capitol Hill',
-  hours: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
   minCustomersHourly: 20,
   maxCustomersHourly: 38,
   avgCookiesPerCustomer: 2.3,
@@ -299,7 +298,6 @@ ul.appendChild(allCookies);
 //start Alki store object, methods, & DOM manipulation
 var alki = {
   name: 'Alki',
-  hours: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
   minCustomersHourly: 2,
   maxCustomersHourly: 16,
   avgCookiesPerCustomer: 4.6,
