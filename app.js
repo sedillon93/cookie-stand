7183b641817612adc2ac7db9cbffb2dd886bc666
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-14 17:33:03 -0700

    fix cookieSaleStatement array

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ var alki = {
   avgCookiesPerCustomer: 4.6,
   hourlySales: [],
   salesStatements: [],
+  total: [],
+  // customersHourly returns a random number of customers
   customersHourly: function(){
     var customers = Math.floor(Math.random() * (this.maxCustomersHourly - this.minCustomersHourly) + this.minCustomersHourly);
     return customers;
   },
+
   // cookiesHourly creates an array of the cookies sold per hour, each element corresponds to sales for one hour; stored in hourlySales
   cookiesHourly: function(){
     for (var i = 0; i < this.hours.length; i++){
@@ -21,29 +24,30 @@ var alki = {
     return this.hourlySales;
   },
 
+    // cookieSaleStatement creates an array of strings of the form 'time am: #ofcookies cookies'; stored in salesStatements
+  cookieSaleStatement: function(){
+    for (var i = 0; i < this.hourlySales.length; i++){
+      if (i < 6) {
+        var message = this.hours[i] + 'am: ' + this.hourlySales[i] + ' cookies';
+      }
+      else {
+        var message = this.hours[i] + 'pm: ' + this.hourlySales[i] + ' cookies';
+      }
+      this.salesStatements.push(message);
+    }
+    return this.salesStatements;
+  },
+
   //add up all numbers in hourlySales array to get total number of cookies sold in a day at that location
   totalCookies: function(){
     total = 0;
     for (var i = 0; i < this.hourlySales.length; i++)
       total += this.hourlySales[i];
-    return total;
+    var message = 'Total: ' + total + ' cookies';
+    this.total.push(message);
   }
 };
 
-  // cookieSaleStatement creates an array of strings of the form 'time am: #ofcookies cookies'; stored in salesStatements
-  // cookieSaleStatement: function(){
-  //   for (var i = 0; i < this.hourlySales.length; i++){
-  //     if (i < 6) {
-  //       var message = this.hours[i] + 'am: ' + this.hourlySales[i] + 'cookies';
-  //     }
-  //     else {
-  //       var message = this.hours[i] + 'pm: ' + this.hourlySales[i] + 'cookies';
-  //     }
-  //     this.salesStatements.push(message);
-  //   }
-  //   return this.salesStatements;
-  // }
-// };
-
 alki.cookiesHourly();
+alki.cookieSaleStatement();
 alki.totalCookies();
