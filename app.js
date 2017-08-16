ee347d76450d14ff8334c4ae1c9a681e3506981e
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-16 15:25:11 -0700

    try to set up function that will generate new instance of Store object using event handler

--- app.js
+++ app.js
@@ -113,23 +113,27 @@ for (var i = 0; i < stores.length; i++){
 }
 footer();
 
-// //attempting to write the function that will create the new store
-// function collectDataAndAppend(event){
-//   //prevent default setting which sends form data to another source; instead keep it here to use it in same page
+// function printThis(event){
 //   event.preventDefault;
-//   //create a new instance of a Store object
-//   var store = new Store();
-//   //assign store's name, min cust, max cust, & avg cookies to a newly created instance
-// store.name =
-// store.minCustomersHourly =
-// store.maxCustomersHourly =
-// store.avgCookiesPerCustomer =
-//   //put new instance into stores array
-// };
-//
-// //what event you are listening for & what to do when it happens
-// //when user hits "create store", it is a submit event
-// //get the form element using its id
-// var form = document.getElementById('form');
-// //when a submit event happens, run the collectAndAppend function
-// form.addEventListener('submit', collectDataAndAppend);
+//   console.log('It works');
+// }
+// var submit = document.getElementById('form');
+// submit.addEventListener('submit', printThis);
+// attempting to write the function that will create the new store
+function collectDataAndAppend(event){
+  event.preventDefault;
+  var store = new Store();
+  store.name = this.elements['name'].value;
+  store.minCustomersHourly = this.elements['minHourly'].value;
+  store.maxCustomersHourly = this.elements['maxHourly'].value;
+  store.avgCookiesPerCustomer = this.elements['avgCookies'].value;
+  store.render();
+  //put new instance into stores array
+};
+
+//what event you are listening for & what to do when it happens
+//when user hits "create store", it is a submit event
+//get the form element using its id
+var form = document.getElementById('form');
+//when a submit event happens, run the collectAndAppend function
+form.addEventListener('submit', collectDataAndAppend);
